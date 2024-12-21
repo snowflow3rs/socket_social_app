e55179b7038680f687ca4da38ffdff6fa0de75b4
Snow flowers <zero2392001@gmail.com>
2024-12-21 16:43:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+
+const app = express();
+
+// Enable CORS for all origins (adjust in production)
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Be more specific in production
+    methods: ["GET", "POST"],
+  },
+});
+let users = [];
+console.log(users);
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+  
+  const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+  };
+  
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  // add user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers",users)
+  });
+
+
+  
+   
+
+// message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    
+    
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  // disconnect
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    removeUser(socket.id)
+    io.emit("getUsers",users)
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
